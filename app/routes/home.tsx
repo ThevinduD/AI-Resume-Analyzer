cb8197279e6adc986c43800dd874e09585d05ba4
ThevinduD <thevindud.work@gmail.com>
2025-07-21 15:46:24 +0530

    feat: Authentication & implement puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,11 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import ResumeCard from "~/components/ResumeCard";
+
+import { resumes } from "constants/index";
+import { usePuterStore } from "~/lib/puter";
+import { useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,14 +15,30 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate("/auth?next=/");
+    }
+  }, [auth.isAuthenticated]);
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-10">
           <h1>Track Your Applications & Resume Ratings</h1>
           <h2>Review your submission and check AI powered feedback</h2>
         </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
       </section>
     </main>
   );
